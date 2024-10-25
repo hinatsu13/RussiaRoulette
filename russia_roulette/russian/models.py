5747be217cbf48ec01ee951bda9d53af6f92c125
Mew_miruku <naegimakoto18147555@gmail.com>
2024-10-25 14:27:06 +0700

    model ok

--- russia_roulette/russian/models.py
+++ russia_roulette/russian/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=10)
+
+class Users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    point = models.IntegerField()
+    phone_number = models.CharField(max_length=10)
+    event = models.ManyToManyField("russian.Event")
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    point_reward = models.IntegerField()
+    admin = models.ForeignKey("russian.Admin", on_delete=models.CASCADE)
+    reward = models.ManyToManyField("russian.Reward")
+
+class Reward(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
